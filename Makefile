b29114fb392865dac43f0ef68f46ab45f193586a
leonardo5621 <leonardo5621@gmail.com>
2022-02-24 15:17:23 -0300

    Change package upvote_pb package name

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 create:
-	protoc --proto_path=proto proto/*.proto --go_out=server/
-	protoc --proto_path=proto proto/*.proto --go-grpc_out=server/
+	protoc --proto_path=upvote_pb upvote_pb/*.proto --go_out=.
+	protoc --proto_path=upvote_pb upvote_pb/*.proto --go-grpc_out=.
 
+get-plugins:
+	go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26
+	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
+	export PATH="$PATH:$(go env GOPATH)/bin"
--- server/upvote.go
+++ server/upvote.go
@@ -4,7 +4,10 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	upvote "upvote_pb"
 	"google.golang.org/grpc"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
@@ -14,16 +17,18 @@ const (
 var collection *mongo.Collection
 
 func main() {
+	options := []grpc.ServerOption{}
+	server := grpc.NewServer(...options)
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatal("failed to listen: %v", err)
 	}
-	mongoClient, err := mongo.NewClient(options.Client().ApplyURI('mongodb://localhost:27017'))
+	mongoClient, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		log.Fatal("Mongo DB connection error: %v", err)
 	}
-	err := mongoClient.Connect(context.TODO())
-	if err != nil {
+	errConnect := mongoClient.Connect(context.TODO())
+	if errConnect != nil {
 		log.Fatal("Mongo DB connection failed: %v", err)
 	}
 	collection = mongoClient.Database("upvote").Collection("vote")
